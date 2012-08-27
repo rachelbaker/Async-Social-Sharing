5edf6b6444f7f49e89c5ff80c561b9d60775b560
Rachel Baker <rachel@rachelbaker.me>
2012-08-27 18:49:02 -0500

    Reworked js file to only call external social scripts that are selected in plugin

--- assets/js/async-share.js
+++ assets/js/async-share.js
@@ -20,22 +20,39 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * ============================================================ */
+jQuery(document).ready(function(d, s, id) {
+    // fb + common
+    var js, fjs = d.getElementsByTagName(s)[0];
+    // facebook
+    if (d.getElementById(id)) {return;}
+    if (jQuery('li.fb-share').length) {
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+    }
+    // tweet
+    if (jQuery('li.twitter-share').length) {
+    js = d.createElement(s);
+    js.src = '//platform.twitter.com/widgets.js';
+    fjs.parentNode.insertBefore(js, fjs);
+    }
+    // +1
+    if (jQuery('li.gplus-share').length) {
+    js = d.createElement(s);
+    js.src = 'https://apis.google.com/js/plusone.js';
+    fjs.parentNode.insertBefore(js, fjs);
+    }
+    // linkedin
+    if (jQuery('li.linkedin-share').length) {
+    js = d.createElement(s);
+    js.src = '//platform.linkedin.com/in.js';
+    fjs.parentNode.insertBefore(js, fjs);
+    }
+    // hackernews
+    if (jQuery('li.hn-share').length) {
+    js = d.createElement(s);
+    js.src = '//hnbutton.appspot.com/static/hn.js';
+    fjs.parentNode.insertBefore(js, fjs);
+    }
 
-jQuery(document).ready(function(w, d, s) {
-  function go(){
-    var js, fjs = d.getElementsByTagName(s)[0], load = function(url, id) {
-          if (d.getElementById(id)) {return;}
-          js = d.createElement(s); js.src = url; js.id = id;
-          fjs.parentNode.insertBefore(js, fjs);
-        };
-    load('//connect.facebook.net/en_US/all.js#xfbml=1', 'fbjssdk');
-    load('//platform.twitter.com/widgets.js', 'tweetjs');
-    load('https://apis.google.com/js/plusone.js', 'gplus1js');
-    load('//platform.linkedin.com/in.js', 'injs');
-    load('//hnbutton.appspot.com/static/hn.js', 'hnjs');
-
-  }
-  if (w.addEventListener) { w.addEventListener("load", go, false); }
-  else if (w.attachEvent) { w.attachEvent("onload",go); }
-
-    }(window, document, 'script'));
+  }(document, 'script'));

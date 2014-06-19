ddfea5a0fecc012233fb16d5b5bdb7342bdd8597
Rachel Baker <rachel@rachelbaker.me>
2014-06-18 21:02:41 -0500

    I shouldn't try to fix things that aren't broken.

--- async-share.php
+++ async-share.php
@@ -153,7 +153,7 @@ class Async_Social_Sharing {
 			ob_start();
 			include( ASYNC_SOCIAL_SHARING_PLUGIN_PATH . 'views/output-view.php' );
 
-			return ob_get_flush();
+			return ob_get_clean();
 		}
 
 		return;
